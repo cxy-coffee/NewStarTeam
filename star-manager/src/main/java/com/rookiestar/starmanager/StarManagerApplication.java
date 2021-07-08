f28cdf7844dfd65b4696932ec39de1d2113196ca
cxy-coffee <1025405845@qq.com>
2021-07-08 11:34:47 +0800

    项目初步框架

---
+++ star-manager/src/main/java/com/rookiestar/starmanager/StarManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rookiestar.starmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarManagerApplication.class, args);
+    }
+
+}
---
+++ star-manager/src/test/java/com/rookiestar/starmanager/StarManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookiestar.starmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
