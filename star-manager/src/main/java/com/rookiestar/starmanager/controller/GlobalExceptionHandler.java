8d4415794085adc8b17d50797e0cde8a76e70827
cxy-coffee <1025405845@qq.com>
2021-07-16 11:58:58 +0800

    修改配置文件，解决部署到阿里云后无法发邮件的问题。

--- star-manager/src/main/java/com/rookiestar/starmanager/controller/GlobalExceptionHandler.java
+++ star-manager/src/main/java/com/rookiestar/starmanager/controller/GlobalExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Description
+ * Handle exception
  *
  * @author 曹向阳
  * @date 2021/7/12
